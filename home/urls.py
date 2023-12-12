ebfe405cd850203d3f5fbfbd69eda8283f3fdca9
yogesh <yv489391@gmail.com>
2023-12-12 22:15:01 +0530

    Completly tracked all files

---
+++ home/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from home import views
+
+
+urlpatterns = [
+    path('', views.index, name="home"),
+    path('login', views.loginUser, name="login"),
+    path('logout', views.logoutUser, name="logout"),
+
+]
